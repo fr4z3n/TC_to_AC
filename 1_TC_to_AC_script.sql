dafc777ec332dc3f2d0b9f41bc7a3f87d9b28dec
fr4z3n <t.baumberger@hotmail.com>
2017-03-01 22:52:57 +0100

    removed todo

--- 1_TC_to_AC_script.sql
+++ 1_TC_to_AC_script.sql
@@ -1,15 +1,3 @@
--- ToDo:
--- Check Spells
--- Check why Engine != InnoDB reason?
--- Check if something is missing
-
--- TC Version: bbc6799cd7c0a83b81496c01d21c110decebae6d
--- AC Version: c92d0403a7047414fcb5901f08da7dd652ca382c
-
--- ToDo if working:
--- Split into different files (each table one file)
--- sh script to generate whole script
-
 CREATE TABLE `character_brew_of_the_month` (
   `guid` INT(10) UNSIGNED NOT NULL,
   `lastEventId` INT(10) UNSIGNED NOT NULL DEFAULT '0',
